cafca3fd07e0578b5045225eda2014426ab00105
INOSUKE29 <259213853+INOSUKE29@users.noreply.github.com>
2026-04-18 02:46:50 +0000

    Automated Supabase Full Backup (DB & Storage) [skip ci]

--- backups/db/data.sql
+++ backups/db/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict s2ofYqoKvWNpZeHxgnJopeJYg3YHyHFdr9FBuYwzpRUU6uDaYANeJN8H0tI06en
+-- \restrict uSogoSIO2G2Ag4F0fjFNW1YGpqBLfOlAVWvTancoldDcjDa2pxAPErk6dGSysJw
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1177,6 +1177,6 @@ SELECT pg_catalog.setval('"public"."usuario_id_quarantine_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict s2ofYqoKvWNpZeHxgnJopeJYg3YHyHFdr9FBuYwzpRUU6uDaYANeJN8H0tI06en
+-- \unrestrict uSogoSIO2G2Ag4F0fjFNW1YGpqBLfOlAVWvTancoldDcjDa2pxAPErk6dGSysJw
 
 RESET ALL;
